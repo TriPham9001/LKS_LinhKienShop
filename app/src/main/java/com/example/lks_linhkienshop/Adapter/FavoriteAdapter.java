e361f4ad5eb8d3df9cbecf6172918bf7dda5af34
TriPham <tri.pham1101@gmail.com>
2022-06-07 15:41:28 +0700

    tri add layout favorite

---
+++ app/src/main/java/com/example/lks_linhkienshop/Adapter/FavoriteAdapter.java
@@ -0,0 +1,77 @@
+package com.example.lks_linhkienshop.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chauthai.swipereveallayout.SwipeRevealLayout;
+import com.chauthai.swipereveallayout.ViewBinderHelper;
+import com.example.lks_linhkienshop.Model.Product;
+import com.example.lks_linhkienshop.R;
+
+import java.util.List;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.FavoriteViewAdapter> {
+    private List<Product> mListProduct;
+    private ViewBinderHelper viewBinderHelper = new ViewBinderHelper();
+
+    public FavoriteAdapter(List<Product> mListProduct) {
+        this.mListProduct = mListProduct;
+    }
+
+    @NonNull
+    @Override
+    public FavoriteViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product_favorite, parent, false);
+        return new FavoriteViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteViewAdapter holder, int position) {
+        Product product = mListProduct.get(position);
+        if (product == null) {
+            return;
+        }
+        holder.imgFavorite.setImageResource(product.getImage());
+        holder.txtNameFavorite.setText(product.getName());
+        holder.txtPriceFavorite.setText(product.getPrice());
+
+        holder.layoutDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListProduct.remove(holder.getAdapterPosition());
+                notifyItemRemoved(holder.getAdapterPosition());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mListProduct != null) {
+            return mListProduct.size();
+        }
+        return 0;
+    }
+
+    public class FavoriteViewAdapter extends RecyclerView.ViewHolder {
+        private RelativeLayout layoutDelete;
+        private ImageView imgFavorite;
+        private TextView txtNameFavorite, txtPriceFavorite;
+
+        public FavoriteViewAdapter(@NonNull View iv) {
+            super(iv);
+            layoutDelete = iv.findViewById(R.id.layoutDelete);
+            imgFavorite = iv.findViewById(R.id.imgFavoriteProduct);
+            txtNameFavorite = iv.findViewById(R.id.txtNameFavorite);
+            txtPriceFavorite = iv.findViewById(R.id.txtPriceFavorite);
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/lks_linhkienshop/Fragment/FavoriteFragment.java
+++ app/src/main/java/com/example/lks_linhkienshop/Fragment/FavoriteFragment.java
@@ -8,13 +8,51 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.lks_linhkienshop.Adapter.FavoriteAdapter;
+import com.example.lks_linhkienshop.Model.Product;
 import com.example.lks_linhkienshop.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FavoriteFragment extends Fragment {
+    private RecyclerView rcvFavoryte;
+    private List<Product> itemListProduct;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorite,container,false);
+        View view =  inflater.inflate(R.layout.fragment_favorite,container,false);
+
+        rcvFavoryte = view.findViewById(R.id.rcvFavorite);
+        rcvFavoryte.setHasFixedSize(true);
+        rcvFavoryte.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        rcvFavoryte.setAdapter(new FavoriteAdapter(initDataProduct()));
+
+        return view;
+
+    }
+
+
+    public List<Product> initDataProduct() {
+        itemListProduct = new ArrayList<>();
+        itemListProduct.add(new Product(R.drawable.cpu,"Mainboard Asus H81M-K","1,390,000","hahahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+        itemListProduct.add(new Product(R.drawable.ic_favorite,"CPU Intel Celeron G5905","1,390,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"RAM Desktop Kingmax 4GB DDR4 Bus 2666MHz","590,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"Ổ cứng HDD 2TB Seagate SkyHawk ST2000VX008","1,440,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"Ổ cứng SSD 256GB ADATA SX6000 (ASX6000LNP-256GT-C)","1,090,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"VGA ASUS DUAL-RTX2060S-O8G-EVO-V2","liên hệ","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"Sound card NEWMB N-EXPS8738","liên hệ","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"VGA ASUS DUAL-RTX2060S-O8G-EVO-V2","370,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"Case Jetek Rhino Art - G9323","liên hệ","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"Nguồn Arrow 625W","440,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"DVDRW Transcend TS8XDVDS-K","750,000","hahah"));
+        itemListProduct.add(new Product(R.drawable.cpu,"Keo tản nhiệt CoolerMaster MASTERGEL REGULAR","130,000","hahah"));
+
+        return itemListProduct;
     }
 }
